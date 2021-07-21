26df2b33ae07dd72964eff7f0632afb52fe3c3c9
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-07-21 12:14:57 +0530

    initial setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { Component } from 'react'
+
+export class App extends Component {
+  render() {
+    return (
+      <>
+        <h1> Hello World :) </h1>
+      </>
+    )
+  }
+}
---
+++ frontend/webpack/webpack.common.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, '..', './src/index.tsx'),
+  },
+  output: {
+    path: path.resolve(__dirname, '..', './build'),
+    filename: '[name].[contenthash].js',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, '..', './src/index.html'),
+      chunks: ['main'],
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  stats: 'errors-only',
+}
