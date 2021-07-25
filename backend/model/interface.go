5eb22028dfd746c54646a70f7e1103f1871529f7
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-07-26 02:40:03 +0530

    added product repo service ports

--- backend/model/interface.go
+++ backend/model/interface.go
@@ -1,10 +1,28 @@
 package model
 
+// UserService must implement the UserService interface
 type UserService interface {
-	Login(*User) (string, error)
 	Signup(*User) error
+	Login(*User) (string, error)
 }
 
+type ProductService interface {
+	Create(*Product) error
+	Delete(ProductID) error
+	FindById(ProductID) (*Product, error)
+	FindByCategory(string) (*Product, error)
+	FindBySeller(SellerID) (*Product, error)
+}
+
+type ProductRepository interface {
+	Create(*Product) error
+	Delete(string) error
+	FindById(string) (*Product, error)
+	FindByCategory(string) (*Product, error)
+	FindBySeller(string) (*Product, error)
+}
+
+// UserRepository must implement the UserRepository interface
 type UserRepository interface {
 	Create(*User) error
 	FindByEmail(string) (*User, error)
