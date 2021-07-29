198efee4f057433f88505c713ad6d680d70ec830
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-07-29 14:17:01 +0530

    added cart service & order service

---
+++ backend/service/cart.go
@@ -0,0 +1,42 @@
+package service
+
+import "github.com/kabi175/e-cart/backend/model"
+
+type CartService struct {
+	cr model.CartRepository
+}
+
+type CartCongig struct {
+	Cr model.CartRepository
+}
+
+func NewCartService(c *CartCongig) model.CartService {
+	return &CartService{
+		cr: c.Cr,
+	}
+}
+
+func (c *CartService) Add(item *model.CartItem) error {
+	err := c.cr.Add(item)
+	return err
+}
+
+func (c *CartService) Remove(item *model.CartItem) error {
+	err := c.cr.Remove(item)
+	return err
+}
+
+func (c *CartService) UpdateUnits(item *model.CartItem) error {
+	err := c.cr.UpdateUnits(item)
+	return err
+}
+
+func (c *CartService) Fetch(user *model.User) ([]*model.CartItem, error) {
+	items, err := c.cr.Fetch(user)
+	return items, err
+}
+
+func (c *CartService) PlaceOrder(user *model.User) error {
+	err := c.cr.PlaceOrder(user)
+	return err
+}
---
+++ backend/service/order.go
@@ -0,0 +1,26 @@
+package service
+
+import "github.com/kabi175/e-cart/backend/model"
+
+type OrderService struct {
+	or model.OrderRepository
+}
+
+type OrderConfig struct {
+	Or model.OrderRepository
+}
+
+func NewOrderService(c *OrderConfig) model.OrderService {
+	return &OrderService{
+		or: c.Or,
+	}
+}
+
+func (o *OrderService) Remove(item *model.OrderItem) error {
+	err := o.or.Remove(item)
+	return err
+}
+func (o *OrderService) Fetch(user *model.User) ([]*model.OrderItem, error) {
+	items, err := o.or.Fetch(user)
+	return items, err
+}
